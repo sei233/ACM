09a9948abccebea1a7c9259ea9535a5d02c7a971
石乾 <1136420833@qq.com>
2018-04-18 14:47:41 +0800

    提交内容：test4想法更正 提交时间：2018/4/18 14：46 提交人：石乾

--- src/test4.java
+++ src/test4.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class test4 {
+public class test4 {                                                          //19-27：最精妙的一段
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
@@ -12,16 +12,16 @@ public class test4 {
             int dp[]=new int[n+100];//以第i个元素结尾的最大
             int start=0,valuestart=0;
             int end=0,valuend=0;
-            for(int i=0;i<n;i++)
+            for(int i=0;i<n;i++)                                              //赋值
             {
                 a[i]=sc.nextInt();
             }
             dp[0]=a[0];
-            int dpmax=a[0];
+            int dpmax=a[0];                                                   //把第一个当作最大值
             for(int i=1;i<n;i++)
             {                                                                   //  从1开始计算？
                 end=i;
-                dp[i]=max(a[i],dp[i-1]+a[i]);
+                dp[i]=max(a[i],dp[i-1]+a[i]);                           //都负a[i]，都正dp[i-1]+a[i]，正负混合←（代码所示）
                 if(a[i]>dp[i-1]+a[i]) {start=i;}                           //前面的数<0,就把起始位置放到i处，类似我的最大值之前全部小于0就不管
                 if(dp[i]>dpmax) {valuestart=start;valuend=end;dpmax=dp[i];}//一定要有等于号
             }
@@ -44,6 +44,7 @@ public class test4 {
 
 //↓想法彻底错误：默认了最大值只有1个：案例 100 100 -101 100，应当为200 1 2
 //改进方法：寻找最优最大值，但是如果有n个最优，算法复杂度*n（放置循环对每个最大值进行一次前一次后），如此，不如从头开始反正从哪里开始都一样
+//但实际上都是类似最大值思路，最大值之前之后变为了，如果sum<0，下一个即作为新起点，如果新起点之后有数之和大于max，新起点即为起点。如果没有，则旧起点为起点
 //import java.util.Scanner;
 //
 //public class test4{
