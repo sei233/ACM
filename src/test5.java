4742e6bee38c85439d7454d9cf94cc1bfcdca971
石乾 <1136420833@qq.com>
2018-04-20 11:01:11 +0800

    提交内容：test5 提交时间：2018/4/20 11：00 提交人：石乾

---
+++ src/test5.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class test5 {
+
+    public static void main(String[] args) {     //wa主要原因为输入不规范
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int t=Integer.parseInt(sc.nextLine());
+            int max = 1;
+            String popular=" ";
+            while (t!=0){
+                String save[] = new String[t];
+                for (int i = 0; i < t; i++) {
+                    String scan = sc.nextLine();
+                    if(i==0)popular=scan;
+                    int num = 1;
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (scan.equals(save[j])) num++;
+                        if (num > max){
+                            max = num;
+                            popular=scan;
+                        }
+                    }
+                    save[i] = scan;
+                }
+                System.out.println(popular);
+                break;
+            }
+            if (t==0)break;
+        }
+    }
+}
+
+//类似下面思路更清晰点
+//import java.util.ArrayList;
+//        import java.util.Scanner;
+//
+//public class t {
+//
+//    public static void main(String args[]){
+//        Scanner s=new Scanner(System.in);
+//        int n;
+//        while((n=Integer.parseInt(s.nextLine()))!=0){
+//            ArrayList<String> color=new ArrayList<String>();   //存放各种颜色以及颜色出现次数得表
+//            int number[]=new int[100];
+//            int k=0;
+//            for(int i=0;i<n;i++){
+//                String str=s.nextLine();
+//                int judge=color.indexOf(str);
+//                if(judge!=-1)
+//                    number[judge]++;
+//                else{
+//                    color.add(k,str);
+//                    number[k]++;
+//                    k++;
+//                }
+//            }
+//            int max=0,maxIndex=0;
+//            for(int i=0;i<k;i++){
+//                if(max<number[i]){
+//                    max=number[i];
+//                    maxIndex=i;
+//                }
+//            }
+//            System.out.println(color.get(maxIndex));
+//        }
+//    }
+//}
